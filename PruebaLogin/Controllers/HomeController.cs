0fd4c6f74906b5b8ee0d947d065f9b8935013e56
Antonio <antonio958@hotmail.com>
2017-08-30 14:19:33 +0200

    Redirige al login

--- PruebaLogin/Controllers/HomeController.cs
+++ PruebaLogin/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace PruebaLogin.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
